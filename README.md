065e07f353761efb1f86436f1a189f253d1e65f9
w0h <54wlh@tongji.edu.cn>
2017-12-02 20:33:36 +0800

    add tmux && change README

--- README.md
+++ README.md
@@ -3,17 +3,32 @@ my dotfiles backup
 
 Modified from some dotfiles I like. :)
 
-I use both KDE and i3wm. (Now I mainly use DDE instead, but dotfiles about i3wm still works).
+I use both KDE and i3wm. (Now I mainly use KDE instead, but dotfiles about i3wm still works).
+
 I hope it can help someone who would like to try i3wm.
 
-### dependencies
+### Dependencies
 - i3-gaps
 - conky
 - compton
 - ttf-font-awesome
 
 You need to install these packages to make my i3 dotfile work.
+
 Hope you like it.
 
-### screenshot
+### Update 2017.12
+I use KDE now and I will switch to i3-gaps when I have a larger monitor...
+
+Now this repo have the following dotfiles and may be different from the screenshot
+
+- zsh ( need oh-my-zsh )
+- neovim
+- tmux
+- i3-gaps, rofi, compton ( still working I guess )
+- editorconfig
+- latte-dock, eclipse preferences (just backup)
+- a script to make dotfiles above work ( It is not tested! )
+
+### Screenshot
 ![screenshot](https://raw.githubusercontent.com/wlh320/wlh-dotfiles/master/screenshot.png)
--- sync.sh
+++ sync.sh
@@ -1,26 +1,48 @@
 #!/bin/sh
+# Only for a clean home directory
+# !!! warning: Don't execute when you have your own dotfiles !!!
+# *** This script haven't been tested! ***
+
 echo "Step 1 : Copy dotfiles"
-git clone https://github.com/wlh320/wlh-dotfiles.git ~/wlh-dotfiles
+git clone https://github.com/wlh320/wlh-dotfiles.git ~/dotfiles
 
-#vimrc
-mv .vimrc .vimrc.bak
-ln -s ~/wlh-dotfiles/vimrc ~/.vimrc
+echo "Step 2 : Install Dependencies"
+sudo pacman -S zsh tmux rofi neovim python-neovim curl wget
 
-#need oh-my-zsh
-mv .zshrc .zshrc.bak
-ln -s ~/wlh-dotfiles/zshrc ~/.zshrc
-cp ~/wlh-dotfiles/myspaceship.zsh-theme ~/.oh-my-zsh/custom/themes
+echo "Step 3 : Link dotfiles"
 
-#vimperator (In fact I no longer use firefox)
-ln -s ~/wlh-dotfiles/vimperator ~/.vimperator
-ln -s ~/wlh-dotfiles/vimperator/.vimperatorrc ~/.vimperatorrc
+# I use nvim instead of vim now
 
-#i3
-ln -s ~/wlh-dotfiles/i3 ~/.i3
-# rofi need this
-ln -s ~/wlh-dotfiles/Xresources ~/.Xresources
+cp -r ~/dotfiles/nvim ~/.config/nvim
+
+# install vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install jellybeans theme:
+wget https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim \
+    -O ~/.config/nvim/colors/jellybeans.vim
+
+# install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# need oh-my-zsh
+
+ln -s ~/dotfiles/zshrc ~/.zshrc
+cp ~/dotfiles/myspaceship.zsh-theme ~/.oh-my-zsh/custom/themes
+
+# vimperator byebye! - since firefox 57
+
+# i3
+ln -s ~/dotfiles/i3 ~/.i3
+
+# rofi
+ln -s ~/dotfiles/Xresources ~/.Xresources
 
 # editorconfig
-ln -s ~/wlh-dotfiles/editorconfig ~/.editorconfig
+ln -s ~/dotfiles/editorconfig ~/.editorconfig
 
-echo "Enjoy it!"
+# tmux
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+
+echo "Done! you executed a dangerous script just now!"
